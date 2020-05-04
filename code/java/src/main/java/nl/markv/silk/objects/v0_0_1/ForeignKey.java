c27bd759c7cf38d4477e981959ca2216267ebfbc
Mark <mverleg.noreply@gmail.com>
2020-05-04 21:53:08 +0200

    Change the format of foreign key (using map)

--- code/java/src/main/java/nl/markv/silk/objects/v0_0_1/ForeignKey.java
+++ code/java/src/main/java/nl/markv/silk/objects/v0_0_1/ForeignKey.java
@@ -2,16 +2,60 @@
 package nl.markv.silk.objects.v0_0_1;
 
 import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 public class ForeignKey implements Serializable
 {
 
-    private final static long serialVersionUID = -246029246207269537L;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @SerializedName("table")
+    @Expose
+    public String table;
+    /**
+     * The mapping of columns, from current table on the left, to target table on the right
+     * (Required)
+     * 
+     */
+    @SerializedName("columns")
+    @Expose
+    public Columns columns;
+    private final static long serialVersionUID = 6073393776538097593L;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public ForeignKey() {
+    }
+
+    /**
+     * 
+     * @param columns
+     * @param table
+     */
+    public ForeignKey(String table, Columns columns) {
+        super();
+        this.table = table;
+        this.columns = columns;
+    }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(ForeignKey.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("table");
+        sb.append('=');
+        sb.append(((this.table == null)?"<null>":this.table));
+        sb.append(',');
+        sb.append("columns");
+        sb.append('=');
+        sb.append(((this.columns == null)?"<null>":this.columns));
+        sb.append(',');
         if (sb.charAt((sb.length()- 1)) == ',') {
             sb.setCharAt((sb.length()- 1), ']');
         } else {
@@ -23,6 +67,8 @@ public class ForeignKey implements Serializable
     @Override
     public int hashCode() {
         int result = 1;
+        result = ((result* 31)+((this.table == null)? 0 :this.table.hashCode()));
+        result = ((result* 31)+((this.columns == null)? 0 :this.columns.hashCode()));
         return result;
     }
 
@@ -35,7 +81,7 @@ public class ForeignKey implements Serializable
             return false;
         }
         ForeignKey rhs = ((ForeignKey) other);
-        return true;
+        return (((this.table == rhs.table)||((this.table!= null)&&this.table.equals(rhs.table)))&&((this.columns == rhs.columns)||((this.columns!= null)&&this.columns.equals(rhs.columns))));
     }
 
 }
