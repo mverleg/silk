be2082de90d49ddd7658c3109b0eea885fdd21e4
Mark <mverleg.noreply@gmail.com>
2020-05-09 15:52:27 +0200

    Enhance type for CheckConstraint

--- parse/java/src/main/java/nl/markv/silk/types/CheckConstraint.java
+++ parse/java/src/main/java/nl/markv/silk/types/CheckConstraint.java
@@ -1,6 +1,11 @@
 
 package nl.markv.silk.types;
 
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
@@ -13,30 +18,22 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 })
 public class CheckConstraint {
 
+    nl.markv.silk.pojos.v0_2_0.CheckConstraint pojo;
+
     @JsonProperty("name")
     public String name;
     /**
      * A boolean sql condition (cross-database syntax)
-     * (Required)
-     * 
      */
     @JsonProperty("condition")
     @JsonPropertyDescription("A boolean sql condition (cross-database syntax)")
     public String condition;
 
-    /**
-     * No args constructor for use in serialization
-     * 
-     */
-    public CheckConstraint() {
-    }
+    public Table table;
 
-    /**
-     * 
-     * @param condition
-     * @param name
-     */
-    public CheckConstraint(String name, String condition) {
+    public CheckConstraint() {}
+
+    public CheckConstraint(@Nullable String name, @Nonnull String condition) {
         super();
         this.name = name;
         this.condition = condition;
@@ -44,42 +41,21 @@ public class CheckConstraint {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(CheckConstraint.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
-        sb.append("name");
-        sb.append('=');
-        sb.append(((this.name == null)?"<null>":this.name));
-        sb.append(',');
-        sb.append("condition");
-        sb.append('=');
-        sb.append(((this.condition == null)?"<null>":this.condition));
-        sb.append(',');
-        if (sb.charAt((sb.length()- 1)) == ',') {
-            sb.setCharAt((sb.length()- 1), ']');
-        } else {
-            sb.append(']');
-        }
-        return sb.toString();
+        return pojo.toString();
     }
 
     @Override
     public int hashCode() {
-        int result = 1;
-        result = ((result* 31)+((this.name == null)? 0 :this.name.hashCode()));
-        result = ((result* 31)+((this.condition == null)? 0 :this.condition.hashCode()));
-        return result;
+        return Objects.hash(table, name, condition);
     }
 
     @Override
     public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-        if ((other instanceof CheckConstraint) == false) {
-            return false;
-        }
+        if (other == this) return true;
+        if (!(other instanceof CheckConstraint)) return false;
         CheckConstraint rhs = ((CheckConstraint) other);
-        return (((this.name == rhs.name)||((this.name!= null)&&this.name.equals(rhs.name)))&&((this.condition == rhs.condition)||((this.condition!= null)&&this.condition.equals(rhs.condition))));
+        return table.equals(rhs.table)
+                && name.equals(rhs.name)
+                && condition.equals(rhs.condition);
     }
-
 }
